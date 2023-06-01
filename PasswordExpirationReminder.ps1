4e1ebef81ffb3ac8290b28c6dc138591e32238f2
Dakota Hourie <VA6DAH@users.noreply.github.com>
2023-06-01 15:27:59 -0600

    Update PasswordExpirationReminder.ps1
    Fixed an issue where the reminder sent a blank toast notification to users if they were not able to reach their domain.

--- PasswordExpirationReminder.ps1
+++ PasswordExpirationReminder.ps1
@@ -1,10 +1,12 @@
 # Author: Dakota Hourie
 # Date Created: 2023-05-30
+# License: MIT License
+# Version: 1.0.2
 
-# Description: This script will check the current users password expiration date and show a toast notification if the password will expire in less than 85 days.
+# Description: This script will check the current users password expiration date and show a toast notification if the password will expire in less than 7 days.
 # The notification will have two buttons, one to change the password and one to dismiss the notification. The notification will persist until the user dismisses it.
 
-# This script is intended to be run as a scheduled task on a Windows 10+ device. The scheduled task should be configured to run as the current user and to run at logon.
+# This script is intended to be run as a scheduled task. This task will also work as a proactive remediation in Intune.
 
 # Start of Script
 
@@ -13,8 +15,14 @@ Install-Module -Name BurntToast -RequiredVersion 0.8.5
 
 # Parameters
 $StartRemindingDays = 7
+$ADDomain = "domain.local"
 
-# Use 'Net User' to retrieve the expiry date of the current users password. Convert this into three variables, Day, Month and Year.
+# Check if there is connectivity to the domain. If there is no connectivity, exit the script.
+if (!(Test-Connection -ComputerName $ADDomain -Count 1 -Quiet)) {
+    exit 418
+}
+
+# Use 'Net User' to retrieve the expiry date of the current users password. Convert this into three variables, Day, Month and Year. If 'System error 1355 has occurred' is returned, the user is not on a domain and the script will exit.
 $PasswordExpirationDate = (net user $env:USERNAME /domain | Select-String "Password expires" | ForEach-Object { $_.ToString().Substring(29) }).TrimEnd(".").Substring(0,10)
 
 # Convert $PasswordeExpirationDate into variables 'day' 'month' and 'year'
